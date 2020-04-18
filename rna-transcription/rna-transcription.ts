09bf46a8dc22385c13c7467d3ec235a18f48457c
smqbit <shyamqbit@gmail.com>
2020-04-18 15:42:19 +0530

    Updated 'rna-transcription' solution

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,30 +1,23 @@
 class Transcriptor {
-  DNA_RNA_MAPPING: Array<string[]> = [
-    ["G", "C"],
-    ["C", "G"],
-    ["T", "A"],
-    ["A", "U"],
-  ];
-
-  DNA_RNA_MAP = new Map();
-
-  constructor() {
-    this.DNA_RNA_MAP = this.DNA_RNA_MAPPING.reduce(
-      (map, [key, value]) => map.set(key, value),
-      this.DNA_RNA_MAP
-    );
-  }
-
   toRna(dna: string) {
-    const result = Array.from(dna).reduce((complement, nucleotide) => {
-      if (!this.DNA_RNA_MAP.has(nucleotide)) {
-        throw new Error("Invalid input DNA.");
-      } else {
-        return `${complement}${this.DNA_RNA_MAP.get(nucleotide)}`;
-      }
-    }, "");
-    return result;
+    const DNA_RNA_MAPPING: DnaRnaMappingInterface = {
+      G: "C",
+      C: "G",
+      T: "A",
+      A: "U",
+    };
+
+    return Array.from(dna)
+      .map((dna) => DNA_RNA_MAPPING[dna] || this.throwError())
+      .join("");
   }
+
+  throwError() {
+    throw new Error("Invalid input DNA.");
+  }
+}
+interface DnaRnaMappingInterface {
+  [key: string]: string;
 }
 
 export default Transcriptor;
