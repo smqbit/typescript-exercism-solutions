0a9b58f2729bab6e3c70833268737c4e32bcfa82
smqbit <shyamqbit@gmail.com>
2020-04-20 22:09:43 +0530

    Added look up in 'resistor-color-duo'

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,18 +1,31 @@
-type Color = "black"| "brown" | "red" | "orange" | "yellow" | "green" | "blue" | "violet" | "grey"  | "white";
+type Color =
+  | "black"
+  | "brown"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "violet"
+  | "grey"
+  | "white";
+type ColorMap = {
+  [key in Color]: number;
+};
 export class ResistorColor {
   private colors: Color[];
-  private colorMap: Color[] = [
-    "black",
-    "brown",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "violet",
-    "grey",
-    "white",
-  ];
+  private colorMap: ColorMap = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
   constructor(colors: Color[]) {
     if (colors.length < 2) {
       throw "At least two colors need to be present";
@@ -20,12 +33,6 @@ export class ResistorColor {
     this.colors = colors.splice(0, 2);
   }
   value = (): number => {
-    return Number(
-      this.colors
-        .map((color) => {
-          return this.colorMap.findIndex((item) => item === color);
-        })
-        .join("")
-    );
+    return Number(this.colors.map((color) => this.colorMap[color]).join(""));
   };
 }
